28e35c0a41f2888b3764b97c4f0dcf22a5a2a3f4
Zach <2000zachary.robinson@gmail.com>
2023-04-29 14:30:33 -0500

    grey-scale images working

---
+++ README.md
@@ -0,0 +1,9 @@
+You need a few libraries installed to make this work:
+pip install numpy
+pip install opencv-python
+pip install PyWavelets
+
+You can run the code on an image by doing the following:
+python wavelet.py <file_path_> [decomp_depth]
+
+It will output a series of images, including the original
--- wavelet.py
+++ wavelet.py
@@ -10,34 +10,34 @@ if len(args) < 2:
     sys.exit()
 
 fileName = args[1]
+depth = args[2] if (len(args) == 3) else 1
 
 
 print("Decomposing Data in", fileName)
 
 # Read in data from file
-image = cv2.imread(fileName)
+image = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
 image = np.float32(image)
 image /= 255
 
+
 # Perform transform
-cA, (cDH, cDV, cDD) = pywt.dwt2(image, "db2")
+array = pywt.wavedec2(image, "db2", level=int(depth))
 
 # Reassemble image
 image *= 255
 image = np.uint8(image)
-cA *= 255
+cA = array[0] * 255
 cA = np.uint8(cA)
-cDH *= 255
-cDH = np.uint8(cDH)
-cDV *= 255
-cDV = np.uint8(cDV)
-cDD *= 255
-cDD = np.uint8(cDD)
-
 cv2.imshow('Original', image)
 cv2.imshow("Analysis", cA)
-cv2.imshow("Horizontal Detail", cDH)
-cv2.imshow("Veritical Detail", cDV)
-cv2.imshow("Diagonal Detail", cDD)
+for i in range(1, len(array)):
+    cH = array[-i][0]
+    cV = array[-i][1]
+    cD = array[-i][2]
+    cv2.imshow("Horizontal" + str(i), cH)
+    cv2.imshow("Veritical" + str(i), cV)
+    cv2.imshow("Diagonal" + str(i), cD)
+
 cv2.waitKey(0)
 
