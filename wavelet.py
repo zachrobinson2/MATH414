f7b6293e1081175b67d5015715cb54888dcede48
Zach <2000zachary.robinson@gmail.com>
2023-05-03 21:36:10 -0500

    Added function generation

--- wavelet.py
+++ wavelet.py
@@ -1,43 +1,54 @@
 import pywt
 import numpy as np
 import sys
-import cv2
+import matplotlib.pyplot as plt
+
+M = 10000
+
+def weierstrass(x, N):
+    y = np.zeros((1,M))
+    for n in range(N+1):
+        y = y + np.cos(3**n*np.pi*x)/2**n
+    return np.reshape(y, (M,))
+
+def s(x):
+    d = x - np.floor(x)
+    return np.minimum(d, 1 - d)
+
+def blancmange_curve(N):
+    def f(x):
+        return sum(s(x * 2 ** n) / 2 ** n for n in range(N))
+    return f
+
+def wiener_process(t, n):
+    dt = t / n
+    dW = np.random.normal(0, np.sqrt(dt), n)
+    W = np.cumsum(dW)
+    return W
 
 args = sys.argv
 
 if len(args) < 2:
-    print("Please enter a filename")
+    print("Please select an option")
     sys.exit()
 
-fileName = args[1]
+option = args[1]
 depth = args[2] if (len(args) == 3) else 1
 
+if option == "1":
+    x = np.linspace(0,2,M)
+    w = weierstrass(x,500)
+    plt.plot(x, w, 'r-')
+    plt.show()
+elif option == "2":
+    x = np.linspace(0,1,M)
+    b = blancmange_curve(500)(x)
+    plt.plot(x, b, 'r-')
+    plt.show()
+elif option == "3":
+    x = np.linspace(0,1,M)
+    y = wiener_process(1, M)
+    plt.plot(x, y, 'r-')
+    plt.show()
 
-print("Decomposing Data in", fileName)
-
-# Read in data from file
-image = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
-image = np.float32(image)
-image /= 255
-
-
-# Perform transform
-array = pywt.wavedec2(image, "db2", level=int(depth))
-
-# Reassemble image
-image *= 255
-image = np.uint8(image)
-cA = array[0] * 255
-cA = np.uint8(cA)
-cv2.imshow('Original', image)
-cv2.imshow("Analysis", cA)
-for i in range(1, len(array)):
-    cH = array[-i][0]
-    cV = array[-i][1]
-    cD = array[-i][2]
-    cv2.imshow("Horizontal" + str(i), cH)
-    cv2.imshow("Veritical" + str(i), cV)
-    cv2.imshow("Diagonal" + str(i), cD)
-
-cv2.waitKey(0)
 
